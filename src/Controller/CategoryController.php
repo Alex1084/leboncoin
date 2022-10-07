7bf154db73f35dffe270ab02d77820ba94fcd2e1
Louce <79318424+Louce@users.noreply.github.com>
2022-10-07 11:07:42 +0200

    Mise en place role admin

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,43 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Category;
-<<<<<<< HEAD
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-#[Route('/category', name: 'app_category')]
-class CategoryController extends AbstractController
-{
-    #[Route('/', name: 'index')]
-    public function index(EntityManagerInterface $em): Response
-    {
-        // $category = new Category();
-        // $category->setName("Mode");
-
-        // $em->persist($category); //Génère la requête pour savoir dans quelle table se placer et quoi afficher
-        // $em->flush(); //affiche la requête
-
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
-        ]);
-    }
-
-    #[Route('/list', name : 'list')]
-    public function list(Request $request, EntityManagerInterface $em, ManagerRegistry $doctrine) : Response
-    {
-        dd($request);
-
-        $categories = $doctrine->getRepository(Category::class)->findAll();
-        // dd($categories); //dump and die
-        dump($categories); //affiche également le contenu de la page
-
-        return $this->render('category/list.html.twig', [
-            'controller_name' => 'CategoryController',
-=======
 use App\Form\CategoryFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
@@ -48,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/category', name: 'app_category_')]
+#[Route('/admin/category', name: 'app_category_')]
 class CategoryController extends AbstractController
 {
 
@@ -94,7 +57,6 @@ class CategoryController extends AbstractController
         }
         return $this->render("/category/form.html.twig", [
             "categoryForm" => $categoryForm->createView()
->>>>>>> 95f8e04fa8d97be25da41b141619bd67572a4d4d
         ]);
     }
 }
